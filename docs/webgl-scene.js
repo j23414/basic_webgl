57406d695f844e9907988c241239803f0ada1239
Jennifer Chang <jennifer.chang.bioinform@gmail.com>
2025-11-09 19:36:23 -0500

    Dark gray background

--- docs/webgl-scene.js
+++ docs/webgl-scene.js
@@ -300,7 +300,7 @@ function initSphereGeometry() {
 function render() {
     // Clear
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    gl.clearColor(0.22, 0.22, 0.22, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
 
